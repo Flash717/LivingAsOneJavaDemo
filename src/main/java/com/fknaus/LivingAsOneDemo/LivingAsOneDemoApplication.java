e79736889f9e6a38dec11a14dcd19d03a11c45c2
Florian Knaus <florian.knaus@gmail.com>
2019-10-03 16:52:10 -0500

    initial commit

---
+++ src/main/java/com/fknaus/LivingAsOneDemo/LivingAsOneDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fknaus.LivingAsOneDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivingAsOneDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivingAsOneDemoApplication.class, args);
+	}
+
+}
